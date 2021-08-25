9862cf5e5ad722a4e28431874e4661885ff7b39d
Florent SICHHIO <rrld8649@EB-L-NRS000139.ad-subs.w2k.francetelecom.fr>
2021-08-25 15:54:20 +0200

    initial commit

---
+++ raceit.py
@@ -0,0 +1,92 @@
+from threading import Barrier, Thread
+import socket
+import ssl
+from http.client import parse_headers, HTTPResponse
+from urllib.parse import urlparse
+import argparse
+from os.path import isfile
+
+def init_socket(host, port):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, port))
+    return s
+
+def init_sslSocket(host, port):
+    context = ssl.create_default_context()
+
+    sock =  socket.create_connection((host, port))
+    ssock = context.wrap_socket(sock, server_hostname=host)
+    return ssock
+
+def run(req, host, port, scheme, barrier):
+    s = init_socket(host, port) if scheme == "http" else init_sslSocket(host, port)
+    s.sendall(req[:-2])
+    barrier.wait()
+    s.sendall(req[-2:])
+
+    res = HTTPResponse(s)
+    res.begin()
+    s.close()
+
+    status = res.status
+    cl = res.getheader("Content-Length")
+
+    print(f"[+] ({status}) content-length: {cl}")
+    
+    return 0
+
+def main():
+
+    desc = '''Just a simple tool to test race condition on engagement.
+    You just take the request intercepted by burp, place it in a file, and provide the url.
+    The tool will generate N thread, each one will initiate a socket with the server, and send all the request except the last byte, and wait using barrier.
+    When all thread has reach the barrier, they all send the last byte at almost same time, that will enhance the probability of race condition.
+    '''
+
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("--thread", "-t",type=int, help="number of thread to use (30 by default)", default=30)
+    parser.add_argument("request_file", type=str, help="file containing the raw HTTP request to race. CAUTION: No integrity check is done on the request and will be send as is.")
+    parser.add_argument("target",type=str, help="the target URL, this one must conatain at least: scheme, hostname (ex: http://localhost:8080).")
+
+    args = parser.parse_args()
+
+    nbThread =  args.thread
+    fReq = args.request_file
+    target = args.target
+
+    url = urlparse(target)
+    if url.scheme in [None, ''] or url.scheme not in ['http', 'https']:
+        print("[-] - URL scheme not supported : %s" % url.scheme)
+        quit()
+    
+    if url.hostname in [None, '']:
+        print("[-] - URL hostname not defined")
+        quit()
+
+
+    if url.port in [None, '']:
+        port = 80 if url.scheme == "http" else 443
+    else:
+        port = url.port
+
+    if not isfile(fReq):
+        print("[-] - request file does not exist.")
+        quit()
+
+    threads = []
+
+    req = open(fReq, "rb").read()
+
+    barrier = Barrier(nbThread)
+    for _ in range(nbThread):
+        t = Thread(target=run, args=(req,url.hostname, port, url.scheme, barrier,))
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
+
+    return 0
+    
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Raceit.py
+
+This is a simple tool I've made to test race condition during engagement.
+
+## How it works :
+
+Inpired by [@bo0om article](https://lab.wallarm.com/race-condition-in-web-applications/), the idea is to initiate multiple thread with a socket for each one, and let them send all the request except last byte. 
+
+When they've send this part, they wait until all thread is ready. When all thread have reach the barrier, they're release and they send the last byte at almost same time. 
+
+This technique give greater chance of race condition, than just sending multiple request in parallele.
+
+## Informations : 
+
+- race condition is "not possible" on PHP session has php use file lock on session by default, so each request is processed one after another
+- the script does not do integrity check on the provided request, so be sure what you put in the file (sorry I'm lazy)
+- race condition is not that documented for web, so if you manage to find cool race condition bug in the wild, please reach me I'm really curious about it :)
+- Burp turbo intruder is probably more effective, but I dont know how it deal with the race condition script, if he just send multiple request in parallele or what, so I preferred to do my own script.
+
+
+*sorry for my bad english and bad coding skills*
